bbcc397b3ccbe14ec78dc2d4c1adc3937467e72e
soreach <heabeoun69zin@gmail.com>
2023-05-04 18:04:05 +0700

    refactor

--- src/autolabel.py
+++ src/autolabel.py
@@ -7,6 +7,7 @@ import os
 import configparser
 import json
 import base64
+import tqdm
 #read global config file
 config = configparser.ConfigParser()
 config.read('auto-labelcfg.ini')
@@ -15,19 +16,21 @@ config.read('auto-labelcfg.ini')
 MODEL_PATH = os.path.join("detect-models",config["MODEL"]["MODEL_NAME"])
 CUDA_DEVICE = select_device(str(config["DEVICE_SETTINGS"]["CUDA_DEVICE"]))
 CONFIDENCE_THRESH = float(config["INFERENCE_CONFIG"]["CONFIDENCE"])
-loadmodel = DetectMultiBackend(MODEL_PATH,CUDA_DEVICE , dnn=False ,data=None ,fp16=False)
+
+
 ext = [".jpeg", ".jpg", ".png"]
-# imgpath = Path("test/test.jpeg")
-# print(os.path.basename(imgpath))
-# det = imageDetect(imgpath, "0", loadmodel, 0.8)
-# det.detect()
 
 class AutoLabel:
     def __init__(self, processingFolder, confidence = 0.9, iou = 0.7):
         self.processingFolder = processingFolder
         self.confidence = confidence
         self.iou = iou
-
+        print("config loaded:")
+        print(f"Model: {MODEL_PATH}\nCuda Device: {CUDA_DEVICE}\nConfidence: {CONFIDENCE_THRESH}")
+        self.loadmodel = DetectMultiBackend(MODEL_PATH,CUDA_DEVICE , dnn=False ,data=None ,fp16=False)
+        if self.loadmodel:
+            print("Model Loaded")
+    
     def findAllImageInFolder(self):
         imagePaths = []
         for roots,dirs,files in os.walk(self.processingFolder):
@@ -40,8 +43,8 @@ class AutoLabel:
 
     def Label(self):
         images = self.findAllImageInFolder()
-        for image in images:
-            detectLabels = imageDetect(image,CUDA_DEVICE,loadmodel,CONFIDENCE_THRESH)
+        for image in tqdm.tqdm(images):
+            detectLabels = imageDetect(image,CUDA_DEVICE,self.loadmodel,CONFIDENCE_THRESH)
             result = detectLabels.detect()
             
             json_obj = json.dumps(result, indent=2)
