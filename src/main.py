30c2f7c6442d4d3b138b0eb05a455245767ee64d
soreach <heabeoun69zin@gmail.com>
2023-05-05 09:03:24 +0700

    typer

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 import os 
 from pathlib import Path
 import typer
+from autolabel import AutoLabel
+from rich import print
 
 
-def main():
-    test = AutoLabel
+def main(source: str = "test"):
+    label = AutoLabel(source)
+    label.Label()
+
+
+if __name__ == "__main__":
+    typer.run(main)
